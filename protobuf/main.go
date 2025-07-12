5b8a412ce561822eba09e0b4f4ca3d344f3ed329
Richard Carson Derr <cakub6@gmx.com>
2025-07-12 16:25:03 -0400

    feat(issue-15): create new module for protobuf

---
+++ protobuf/main.go
@@ -0,0 +1,107 @@
+// Copyright (c) 2025 Z5Labs and Contributors
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+
+	"dagger/protobuf/internal/dagger"
+
+	"github.com/containerd/platforms"
+)
+
+type Protobuf struct {
+	Container *dagger.Container
+
+	// +private
+	Plaform platforms.Platform
+}
+
+func New(
+	ctx context.Context,
+	version string,
+	// +optional
+	platform dagger.Platform,
+	// +optional
+	from *dagger.Container,
+) (*Protobuf, error) {
+	if platform == "" {
+		platform = dagger.Platform(platforms.DefaultString())
+	}
+
+	p, err := platforms.Parse(string(platform))
+	if err != nil {
+		return nil, err
+	}
+
+	if from == nil {
+		c, err := newContainer(version, p)
+		if err != nil {
+			return nil, err
+		}
+
+		from = c
+	}
+
+	return &Protobuf{
+		Container: from,
+		Plaform:   p,
+	}, nil
+}
+
+func newContainer(version string, platform platforms.Platform) (*dagger.Container, error) {
+	arch, err := mapToProtoArch(platform.Architecture)
+	if err != nil {
+		return nil, err
+	}
+
+	protoc := dag.HTTP(fmt.Sprintf(
+		"https://github.com/protocolbuffers/protobuf/releases/download/%s/protoc-%s-%s-%s.zip",
+		version,
+		strings.TrimPrefix(version, "v"),
+		platform.OS,
+		arch,
+	))
+
+	c := dag.Container().
+		WithFile("/bin/protoc", protoc)
+
+	return c, nil
+}
+
+func mapToProtoArch(arch string) (string, error) {
+	mapping := map[string]string{
+		"amd64":   "x86_64",
+		"ppc64le": "ppcle_64",
+	}
+
+	mappedArch, ok := mapping[arch]
+	if !ok {
+		return "", errors.New("protoc does not support: " + arch)
+
+	}
+
+	return mappedArch, nil
+}
+
+// Register the given binary file as a protoc plugin.
+func (m *Protobuf) WithPlugin(name string, bin *dagger.File) (*Protobuf, error) {
+	if strings.HasPrefix(name, "protoc-gen-") {
+		return nil, errors.New("plugin name must start with: protoc-gen-")
+	}
+
+	m.Container = m.Container.WithFile("/protobuf/plugin/"+name, bin)
+
+	return m, nil
+}
+
+// Copy well-known types, protoc, and plugins to the provided container.
+func (m *Protobuf) CopyTo(container *dagger.Container) *dagger.Container {
+	return container
+}
